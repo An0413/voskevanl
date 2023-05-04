a615cf4d0f5a2454bc16e051090ddc46597d2c4a
An0413 <aleqsanyan.an@bk.ru>
2023-05-04 12:25:07 +0400

    Voskevan kayq

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Main\IndexController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::group(['namespase' => 'Main'], function (){
+    Route::get('/',  IndexController::class)->name('layouts.main');
+});
+
+
+Auth::routes();
+
