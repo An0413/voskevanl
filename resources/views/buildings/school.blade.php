f25894a2c7dcda3123bcf29fe7943671cb7aed67
An0413 <aleqsanyan.an@bk.ru>
2023-05-20 00:23:26 +0400

    change school

--- resources/views/buildings/school.blade.php
+++ resources/views/buildings/school.blade.php
@@ -3,105 +3,56 @@
 @yield('header')
 
 
-{{--<div class="container mt-5">--}}
-    {{--    <div class="mt-5">--}}
-    {{--        <h1 class="text-center" style="margin-top: 10%">Ոսկեվանի միջնակարգ դպրոց</h1>--}}
-    {{--    </div>--}}
-    {{--    <div>--}}
-    {{--        <img src="{{asset('assets/img/school/dproc.jpg')}}" class="dproc_img">--}}
-    {{--    </div>--}}
 
-    {{--    <div>--}}
-    {{--        <?php foreach ($school as $value) { ?>--}}
-    {{--        <div>--}}
-    {{--            <h3 class="text-center" style="font-family: Sofia;"><?php echo $value['name']; ?></h3>--}}
-    {{--        </div>--}}
-    {{--        <p style="text-indent: 3%; padding: 1%;"><?php echo $value['description'] ?></p>--}}
-    {{--        <?php--}}
-    {{--        }--}}
-    {{--        ?>--}}
-    {{--    </div>--}}
-    {{--    <div class="row mt-5">--}}
-    {{--        <div>--}}
-    {{--            <h3 class="text-center mt-5" style="font-family: Sofia">Դպրոցի առաջին շրջավարտները</h3>--}}
-    {{--        </div>--}}
-    {{--        <div class="col-3 mt-5">--}}
-    {{--            <h5 style="padding-left: 10%; font-family: Sofia"><b>Ոսկեվան</b></h5>--}}
-    {{--            <ul>--}}
-    {{--                <?php--}}
-    {{--                foreach ($voskevan as $value) {--}}
-    {{--                ?>--}}
-    {{--                <li><?php echo $value['name'] . ' ';--}}
-    {{--                    echo $value['lastname']; ?>--}}
-    {{--                </li>--}}
-    {{--                <?php--}}
-    {{--                }--}}
-    {{--                ?>--}}
-    {{--            </ul>--}}
-    {{--        </div>--}}
-    {{--        <div class="col-3 mt-5">--}}
-    {{--            <h5 style="padding-left: 10%; font-family: Sofia"><b>Բաղանիս</b></h5>--}}
-    {{--            <ul>--}}
-    {{--                <?php--}}
-    {{--                foreach ($baxanis as $value) {--}}
-    {{--                ?>--}}
-    {{--                <li><?php echo $value['name'] . ' ';--}}
-    {{--                    echo $value['lastname']; ?>--}}
-    {{--                </li>--}}
-    {{--                <?php--}}
-    {{--                }--}}
-    {{--                ?>--}}
-    {{--            </ul>--}}
-    {{--        </div>--}}
-    {{--        <div class="col-3 mt-5">--}}
-    {{--            <h5 style="padding-left: 10%; font-family: Sofia"><b>Բարեկամավան</b></h5>--}}
-    {{--            <ul>--}}
-    {{--                <?php--}}
-    {{--                foreach ($barekamavan as $value) {--}}
-    {{--                ?>--}}
-    {{--                <li><?php echo $value['name'] . ' ';--}}
-    {{--                    echo $value['lastname']; ?>--}}
-    {{--                </li>--}}
-    {{--                <?php--}}
-    {{--                }--}}
-    {{--                ?>--}}
-    {{--            </ul>--}}
-    {{--        </div>--}}
-    {{--        <div class="col-3 mt-5">--}}
-    {{--            <h5 style="padding-left: 10%; font-family: Sofia"><b>Կոթի</b></h5>--}}
-    {{--            <ul>--}}
-    {{--                <?php--}}
-    {{--                foreach ($koti as $value) {--}}
-    {{--                ?>--}}
-    {{--                <li><?php echo $value['name'] . ' ';--}}
-    {{--                    echo $value['lastname']; ?>--}}
-    {{--                </li>--}}
-    {{--                <?php--}}
-    {{--                }--}}
-    {{--                ?>--}}
-    {{--            </ul>--}}
-    {{--            <h5 style="padding-left: 10%; font-family: Sofia;"><b>Ջուջեվան</b></h5>--}}
-    {{--            <ul>--}}
-    {{--                <?php--}}
-    {{--                foreach ($jujevan as $value) {--}}
-    {{--                ?>--}}
-    {{--                <li><?php echo $value['name'] . ' ';--}}
-    {{--                    echo $value['lastname']; ?>--}}
-    {{--                </li>--}}
-    {{--                <?php--}}
-    {{--                }--}}
-    {{--                ?>--}}
-    {{--            </ul>--}}
-    {{--        </div>--}}
-    {{--    </div>--}}
-    {{--    <div>--}}
-    {{--        <h3 class="text-center mt-5" style="font-family: Sofia">Ներկա պահին դպրոցի ուսուցչական կազմը</h3>--}}
-    {{--    </div>--}}
+{{--        <div class="mt-5">--}}
+{{--            <h1 class="text-center" style="margin-top: 10%; margin-bottom: 3%">Ոսկեվանի միջնակարգ դպրոց</h1>--}}
+{{--        </div>--}}
+{{--        <div>--}}
+{{--            <img src="{{asset('assets/img/school/dproc.jpg')}}" style="width: 85%; margin-left: 8%">--}}
+{{--        </div>--}}
+
+
+<section id="portfolio" class="portfolio">
+    <div class="container">
+
+        <div class="section-title aos-init aos-animate" data-aos="fade-in" data-aos-delay="100">
+            <h2>ՈՍԿԵՎԱՆԻ ՄԻՋՆԱԿԱՐԳ ԴՊՐՈՑ</h2>
+        </div>
+
+        <div class="row aos-init aos-animate" data-aos="fade-in">
+            <div class="col-lg-12 d-flex justify-content-center">
+                <ul id="portfolio-flters">
+                    <li data-filter="*" class="filter-active">All</li>
+                    <li data-filter=".filter-app">App</li>
+                    <li data-filter=".filter-card">Card</li>
+                    <li data-filter=".filter-web">Web</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="row portfolio-container aos-init aos-animate" data-aos="fade-up" style="position: relative; height: 1025.96px;">
+            @foreach ($images as $value)
+            <div class="col-lg-4 col-md-6 portfolio-item filter-app" style="position: absolute; left: 0px; top: 0px;">
+                <div class="portfolio-wrap">
+                    <img src="{{asset('assets/img/school/'. $value['src'])}}" class="img-fluid">
+                    <div class="portfolio-links">
+                        <a href="{{asset('assets/img/school'. $value['src'])}}" data-gallery="portfolioGallery" class="portfolio-lightbox"><i class="bx bx-plus"></i></a>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+
+        </div>
+
+    </div>
+</section>
+
+
+
     <section is="team" class="team">
         <div class="container">
             <div class="section-title aos-init aos-animate" data-aos="fade-in" data-aos-delay="100">
-                <h2>Team</h2>
-                <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+                <h2>ԱՇԽԱՏԱԿԱԶՄ</h2>
             </div>
             <div class="row mt-5 mb-5">
                 @foreach ($worker as $value)
@@ -123,18 +74,6 @@
                             </div>
                         </div>
                     </div>
-
-
-                    {{--        <div class="col-3">--}}
-                    {{--            <div class="card" style="width:280px; margin-left: 2%; margin-bottom: 2%; height: 450px">--}}
-                    {{--                <img class="card-img-top" src="{{asset('assets/img/school/'. $value['img'])}}" alt="Card image" style="width: 95%; height: 70%%">--}}
-                    {{--                <div class="card-body">--}}
-                    {{--                    <h4 class="card-title"><?php echo $value['name'] . '  ';--}}
-                    {{--                        echo $value['lastname']; ?></h4>--}}
-                    {{--                    <p class="card-text "><?php echo $value['position'] ?></p>--}}
-                    {{--                </div>--}}
-                    {{--            </div>--}}
-                    {{--        </div>--}}
                 @endforeach
             </div>
         </div>
