2667d704f918d3368cf58429291c06ec63f04b68
An0413 <aleqsanyan.an@bk.ru>
2023-05-22 12:16:23 +0400

    schholverjnakan

--- resources/views/buildings/school.blade.php
+++ resources/views/buildings/school.blade.php
@@ -1,10 +1,31 @@
 @include('layouts.header')
 
 @yield('header')
+<div class="container">
+<div class="mt-5">
+    <h1 class="text-center" style="margin-top: 10%">Ոսկեվանի միջնակարգ դպրոց</h1>
+</div>
 
 
+<section id="cta" class="cta">
+    <div class="container aos-init aos-animate" data-aos="zoom-in">
 
+        <div class="text-center">
+            <?php foreach ($school as $value) { ?>
+            <div>
+                <h3 class="text-center" style="font-family: Sofia;"><?php echo $value['name']; ?></h3>
+            </div>
+            <p style="text-indent: 3%; padding: 1%;"><?php echo $value['description'] ?></p>
+            <?php
+            }
+            ?>
+            <a class="cta-btn" href="#">Կարդալ ավելին</a>
+        </div>
+    </div>
+    <img src="{{asset('assets/img/school/dproc.jpg')}}"
+         style="width: 80%; margin-left: 10%; margin-top: 2%">
 
+</section>
 
 <section id="portfolio" class="portfolio">
     <div class="container">
@@ -13,98 +34,56 @@
             <h2>ԼՈՒՍԱՆԿԱՐՆԵՐ</h2>
         </div>
 
-{{--        <div class="row aos-init aos-animate" data-aos="fade-in">--}}
-{{--            <div class="col-lg-12 d-flex justify-content-center">--}}
-{{--                <ul id="portfolio-flters">--}}
-{{--                    <li data-filter="*" class="filter-active">All</li>--}}
-{{--                    <li data-filter=".filter-app">App</li>--}}
-{{--                    <li data-filter=".filter-card">Card</li>--}}
-{{--                    <li data-filter=".filter-web">Web</li>--}}
-{{--                </ul>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-
-        <div class="row portfolio-container aos-init aos-animate" data-aos="fade-up" style="position: relative; height: 1025.96px;">
+        <div class="row portfolio-container aos-init aos-animate" data-aos="fade-up"
+             style="position: relative; height: 1025.96px;">
 
             @foreach ($images as $value)
-                <div class="col-lg-4 col-md-6 portfolio-item filter-app" style="position: absolute; left: 0px; top: 0px;">
+                <div class="col-lg-4 col-md-6 portfolio-item filter-app">
                     <div class="portfolio-wrap">
                         <img src="{{asset('assets/img/school/'. $value['src'])}}" class="img-fluid">
                         <div class="portfolio-links">
-                            <a href="{{asset('assets/img/school'. $value['src'])}}" data-gallery="portfolioGallery" class="portfolio-lightbox"><i class="bx bx-plus"></i></a>
+                            <a href="{{asset('assets/img/school/'. $value['src'])}}" data-gallery="portfolioGallery"
+                               class="portfolio-lightbox"><i class="bx bx-plus"></i></a>
                         </div>
                     </div>
                 </div>
             @endforeach
-{{--            <div class="col-lg-4 col-md-6 portfolio-item filter-card" style="position: absolute; left: 0px; top: 683.974px;">--}}
-{{--                <div class="portfolio-wrap">--}}
-{{--                    <img src="assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="">--}}
-{{--                    <div class="portfolio-links">--}}
-{{--                        <a href="assets/img/portfolio/portfolio-7.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 1"><i class="bx bx-plus"></i></a>--}}
-{{--                        <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-
-{{--            <div class="col-lg-4 col-md-6 portfolio-item filter-card" style="position: absolute; left: 439.987px; top: 683.974px;">--}}
-{{--                <div class="portfolio-wrap">--}}
-{{--                    <img src="assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="">--}}
-{{--                    <div class="portfolio-links">--}}
-{{--                        <a href="assets/img/portfolio/portfolio-8.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 3"><i class="bx bx-plus"></i></a>--}}
-{{--                        <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-
-{{--            <div class="col-lg-4 col-md-6 portfolio-item filter-web" style="position: absolute; left: 879.974px; top: 683.974px;">--}}
-{{--                <div class="portfolio-wrap">--}}
-{{--                    <img src="assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="">--}}
-{{--                    <div class="portfolio-links">--}}
-{{--                        <a href="assets/img/portfolio/portfolio-9.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>--}}
-{{--                        <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-
         </div>
 
     </div>
 </section>
 
-    <section is="team" class="team">
-        <div class="container">
-            <div class="section-title aos-init aos-animate" data-aos="fade-in" data-aos-delay="100">
-                <h2>ԱՇԽԱՏԱԿԱԶՄ</h2>
-            </div>
-            <div class="row mt-5 mb-5">
-                @foreach ($worker as $value)
-                    <div class="col-lg-4 col-md-6 mt-5">
-                        <div class="member aos-init aos-animate" data-aos="fade-up">
-                            <div class="pic">
-                                <img src="{{asset('assets/img/school/'. $value['img'])}}" class="img-fluid"
-                                                  alt="" style="height: 300px">
-                            </div>
-                            <div class="member-info">
-                                <h4>{{$value->name . ' '. $value->lastname}}</h4>
-                                <span>{{$value->positions->title}}</span>
-                                <div class="social">
-                                    <a href=""><i class="bi bi-twitter"></i></a>
-                                    <a href=""><i class="bi bi-facebook"></i></a>
-                                    <a href=""><i class="bi bi-instagram"></i></a>
-                                    <a href=""><i class="bi bi-linkedin"></i></a>
-                                </div>
+<section is="team" class="team">
+    <div class="container">
+        <div class="section-title aos-init aos-animate" data-aos="fade-in" data-aos-delay="100">
+            <h2>ԱՇԽԱՏԱԿԱԶՄ</h2>
+        </div>
+        <div class="row mt-5 mb-5">
+            @foreach ($worker as $value)
+                <div class="col-lg-4 col-md-6 mt-5">
+                    <div class="member aos-init aos-animate" data-aos="fade-up">
+                        <div class="pic">
+                            <img src="{{asset('assets/img/school/'. $value['img'])}}" class="img-fluid"
+                                 alt="" style="height: 300px">
+                        </div>
+                        <div class="member-info">
+                            <h4>{{$value->name . ' '. $value->lastname}}</h4>
+                            <span>{{$value->positions->title}}</span>
+                            <div class="social">
+                                <a href=""><i class="bi bi-twitter"></i></a>
+                                <a href=""><i class="bi bi-facebook"></i></a>
+                                <a href=""><i class="bi bi-instagram"></i></a>
+                                <a href=""><i class="bi bi-linkedin"></i></a>
                             </div>
                         </div>
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @endforeach
         </div>
-    </section>
+    </div>
+</section>
 
-
-
-
-
+</div>
 @include('layouts.footer')
 
 @yield('footer')
