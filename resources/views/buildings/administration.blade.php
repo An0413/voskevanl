802e509792f56d93496b398e56bbd778bd7d82da
AniSargsyan97 <anisargsyan.9697@mail.ru>
2023-05-22 10:07:15 +0400

    ambulance changes

--- resources/views/buildings/administration.blade.php
+++ resources/views/buildings/administration.blade.php
@@ -4,13 +4,13 @@
 
 
 <div class="container">
-    <h1 class="text-center administration_header">Գյուղապետարան</h1>
+    <h1 class="text-center administration_header mt-5">Գյուղապետարան</h1>
     <div class="row mt-5 n_p p-5">
         <div class="col-6">
-                @foreach($administration as $value)
-                    <h3>{{ $value->title }}</h3>
-                    <p>{{ $value->content }}</p>
-                @endforeach
+            @foreach($administration as $value)
+                <h3>{{ $value->title }}</h3>
+                <p>{{ $value->content }}</p>
+            @endforeach
 
         </div>
         <div class="col-6">
@@ -22,16 +22,36 @@
 
     <h3 class="patm"> Ոսկեվանի գյուղապետարանի աշխատակազմը</h3>
     <div class="row">
-        @foreach ($worker as $value)
-        <div class="col-sm-3 card heru mt-4">
-            <img class="card-img-top" src="{{asset('assets/img/kindergarten/'.$value->img)}}" alt="Card image"
-                 style="width:100%">
-            <div class="card-body">
-                <h4 class="card-title">{{$value->name}}</h4>
-                <h4 class="card-title">{{$value->lastname}}</h4>
-                <h4 class="card-title">{{$value->positions->title}}</h4>
+        {{--        @foreach ($worker as $value)--}}
+        {{--            <div class="col-sm-3 card heru mt-4">--}}
+        {{--                <img class="card-img-top" src="{{asset('assets/img/kindergarten/'.$value->img)}}" alt="Card image"--}}
+        {{--                     style="width:100%">--}}
+        {{--                <div class="card-body">--}}
+        {{--                    <h4 class="card-title">{{$value->name}}</h4>--}}
+        {{--                    <h4 class="card-title">{{$value->lastname}}</h4>--}}
+        {{--                    <h4 class="card-title">{{$value->positions->title}}</h4>--}}
+        {{--                </div>--}}
+        {{--            </div>--}}
+        {{--        @endforeach--}}
+        {{--    </div>--}}
+
+        @foreach($worker as $value)
+            <div class="col-lg-4 col-md-6">
+                <div class="member" data-aos="fade-up">
+                    <div class="pic"><img src="{{asset('assets/img/kindergarten/'.$value->img)}}" class="img-fluid"
+                                          alt=""></div>
+                    <div class="member-info">
+                        <h4>{{$value->name}} {{$value->lastname}}</h4>
+                        <span>{{$value->positions->title}}</span>
+                        <div class="social">
+                            <a href=""><i class="bi bi-twitter"></i></a>
+                            <a href=""><i class="bi bi-facebook"></i></a>
+                            <a href=""><i class="bi bi-instagram"></i></a>
+                            <a href=""><i class="bi bi-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
         @endforeach
     </div>
 </div>
--- resources/views/buildings/ambulance.blade.php
+++ resources/views/buildings/ambulance.blade.php
@@ -3,35 +3,133 @@
 @yield('header')
 
 
-<div class="container">
-    <h1 class="text-center administration_header">Ամբուլատորիա</h1>
-    <div class="row mt-5 n_p p-5">
-        <div class="col-6">
-                @foreach($ambulance as $value)
-                    <h3>{{ $value->title }}</h3>
-                    <p>{{ $value->content }}</p>
-                @endforeach
+<div class="container-fluid">
+    {{--    <h1 class="text-center administration_header">Ամբուլատորիա</h1>--}}
+    {{--    <div class="row mt-5 n_p p-5">--}}
+    {{--        <div class="col-6">--}}
+    {{--                @foreach($ambulance as $value)--}}
+    {{--                    <h3>{{ $value->title }}</h3>--}}
+    {{--                    <p>{{ $value->content }}</p>--}}
+    {{--                @endforeach--}}
 
-        </div>
-        <div class="col-6">
-            <img src="{{asset('assets/img/buildings/ambulatoria.jpg')}}"
-                 style="width: 90%;margin-left: 10%; margin-bottom: 6%">
-        </div>
+    {{--        </div>--}}
+    {{--        <div class="col-6">--}}
+    {{--            <img src="{{asset('assets/img/buildings/ambulatoria.jpg')}}"--}}
+    {{--                 style="width: 90%;margin-left: 10%; margin-bottom: 6%">--}}
+    {{--        </div>--}}
+
+    <div class="cross">
+
 
     </div>
+
+    <div>
+        <h3 class="patm mt-5">Լուսանկարներ</h3>
+    </div>
+    <section id="portfolio" class="portfolio">
+        <div class="container">
+
+            <div class="row" data-aos="fade-in">
+                <div class="col-lg-12 d-flex justify-content-center">
+                    <ul id="portfolio-flters">
+                        <li data-filter="*" class="filter-active">All</li>
+                        <li data-filter=".filter-app">App</li>
+                        <li data-filter=".filter-card">Card</li>
+                        <li data-filter=".filter-web">Web</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="row portfolio-container" data-aos="fade-up">
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                    <div class="portfolio-wrap">
+                        <img src="{{asset('assets/img/buildings/amb.jpg')}}" class="img-fluid" alt="">
+                        <div class="portfolio-links">
+                            <a href="{{asset('assets/img/buildings/amb.jpg')}}" data-gallery="portfolioGallery" class="portfolio-lightbox"><i class="bx bx-plus"></i></a>
+                            <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+                    <div class="portfolio-wrap">
+                        <img src="{{asset('assets/img/buildings/pills.jpg')}}" class="img-fluid" alt="">
+                        <div class="portfolio-links">
+                            <a href="{{asset('assets/img/buildings/pills.jpg')}}" data-gallery="portfolioGallery" class="portfolio-lightbox" ><i class="bx bx-plus"></i></a>
+                            <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                    <div class="portfolio-wrap">
+                        <img src="{{asset('assets/img/portfolio/portfolio-3.jpg')}}" class="img-fluid" alt="">
+                        <div class="portfolio-links">
+                            <a href="{{asset('assets/img/portfolio/portfolio-3.jpg')}}" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 2"><i class="bx bx-plus"></i></a>
+                            <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+                    <div class="portfolio-wrap">
+                        <img src="{{asset('assets/img/portfolio/portfolio-4.jpg')}}" class="img-fluid" alt="">
+                        <div class="portfolio-links">
+                            <a href="{{asset('assets/img/portfolio/portfolio-4.jpg')}}" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
+                            <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+                    <div class="portfolio-wrap">
+                        <img src="{{asset('assets/img/portfolio/portfolio-5.jpg')}}" class="img-fluid" alt="">
+                        <div class="portfolio-links">
+                            <a href="{{asset('assets/img/portfolio/portfolio-5.jpg')}}" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 2"><i class="bx bx-plus"></i></a>
+                            <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                    <div class="portfolio-wrap">
+                        <img src="{{asset('assets/img/portfolio/portfolio-6.jpg')}}" class="img-fluid" alt="">
+                        <div class="portfolio-links">
+                            <a href="{{asset('assets/img/portfolio/portfolio-6.jpg')}}" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 3"><i class="bx bx-plus"></i></a>
+                            <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section><!-- End Portfolio Section -->
+
+
+
+
 
     <h3 class="patm mt-5"> Ոսկեվանի ամբուլատորիայի աշխատակազմը</h3>
     <div class="row">
         @foreach ($worker as $value)
-        <div class="col-sm-3 card heru mt-5">
-            <img class="card-img-top" src="{{asset('assets/img/kindergarten/'.$value->img)}}" alt="Card image"
-                 style="width:100%">
-            <div class="card-body">
-                <h3 class="card-title">{{$value->name}}</h3>
-                <h3 class="card-title">{{$value->lastname}}</h3>
-                <h5 class="card-title">{{$value->positions->title}}</h5>
+            <div class="col-lg-4 col-md-6">
+                <div class="member" data-aos="fade-up" data-aos-delay="300">
+                    <div class="pic"><img src="{{asset('assets/img/kindergarten/'.$value->img)}}" class="img-fluid"
+                                          alt=""></div>
+                    <div class="member-info">
+                        <h4>{{$value->name}}  {{$value->lastname}}</h4>
+                        <span>{{$value->positions->title}}</span>
+                        <div class="social">
+                            <a href=""><i class="bi bi-twitter"></i></a>
+                            <a href=""><i class="bi bi-facebook"></i></a>
+                            <a href=""><i class="bi bi-instagram"></i></a>
+                            <a href=""><i class="bi bi-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
         @endforeach
     </div>
 </div>
