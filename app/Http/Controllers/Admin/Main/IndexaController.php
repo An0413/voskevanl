d0f7719a931a182f96fa4deb850ad4ee35fc3c6b
An0413 <aleqsanyan.an@bk.ru>
2023-06-15 08:09:53 +0400

    hastatman ej/anavart

--- app/Http/Controllers/Admin/Main/IndexaController.php
+++ app/Http/Controllers/Admin/Main/IndexaController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin\Main;
 
 use App\Http\Controllers\Controller;
+use App\Models\Images;
+use App\Models\Main_info;
 use App\Models\User;
 use App\Models\Worker;
 use Illuminate\Http\Request;
@@ -12,15 +14,21 @@ use App\Helper;
 
 class IndexaController extends Controller
 {
-    public function index()
+    public function index($worker_id)
     {
 
         if (!Auth::user()){
             return redirect('admin/login');
         }
+        if (Auth::)
+        $worker = Worker::where('worker_id',  $worker_id)->where('status', 2)->get();
+
+        $images = Images::where('gallery_id',  $worker_id)->where('status', 2)->get();
+
+        $info = Main_info::where('menu_id', $worker_id)->where('status', 2)->get();
+
         $admin_info = Helper::getAdmin();
 
-        $worker = Worker::where('worker_id', '=', 5)->get();
-        return view('admin.main.index', compact('worker', 'admin_info'));
+        return view('admin.main.index', compact('worker', 'admin_info','images', 'info', 'worker_id'));
     }
 }
